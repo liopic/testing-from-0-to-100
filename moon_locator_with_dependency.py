05ea00396a5ad3665e5e6b1817bcbd1491068155
Julio Martinez <liopic@liopic.com>
2023-04-26 18:28:40 +0200

    fix: wrong code example in case with dependency

--- moon_locator_with_dependency.py
+++ moon_locator_with_dependency.py
@@ -2,14 +2,18 @@ from datetime import datetime, timedelta
 from math import ceil
 
 
+def reference_date_for_year(year) -> datetime:
+    if year >= 2022:
+        return datetime(2022, 11, 15, 6, 1, 12)
+    else:
+        return datetime(2010, 1, 2, 2, 32, 4)
+
+
 class MoonLocator:
-    moon_on_south = datetime(year=2022, month=11, day=15, hour=6, minute=1, second=12)
     moon_interval = timedelta(days=1, hours=0, minutes=50, seconds=28)
 
     def next_moon_on_south_from_date(self, from_date: datetime) -> datetime:
-        since_moon_on_south = from_date - self.moon_on_south
+        moon_on_south = reference_date_for_year(from_date.year)
+        since_moon_on_south = from_date - moon_on_south
         cycle = ceil(since_moon_on_south / self.moon_interval)
-        return self.moon_on_south + self.moon_interval * cycle
-
-    def other_untested_function() -> int:
-        0
+        return moon_on_south + self.moon_interval * cycle
